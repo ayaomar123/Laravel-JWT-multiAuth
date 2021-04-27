f692435cd518a2fd0ab71881b8743334fba567fd
aya omar <yotayot29@gmail.com>
2021-04-27 13:24:22 +0300

    Aya Omar 27-4 1.24

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/user/register', [UserController::class, 'register']);
+Route::post('/user/login', [UserController::class, 'login']);
+
+//guard:users
+Route::group(['middleware' => ['auth:users']], function () {
+    Route::post('/user/signout', [UserController::class, 'logout']);
+    Route::post('/user/updateProfile', [UserController::class, 'updateProfile']);
+    Route::post('/user/editPassword', [UserController::class, 'editPassword']);
+});
+
+
+
+Route::post('/admin/register', [AdminController::class, 'register']);
+
+Route::post('/admin/login', [AdminController::class, 'login']);
+
+Route::group(['middleware' => ['auth:admins']], function () {
+    Route::post('/admin/logout', [AdminController::class, 'logout']);
+    Route::get('/admin/getAllUser', [AdminController::class, 'getAllUser']);
+    Route::post('/admin/updateProfile', [AdminController::class, 'updateProfile']);
+    Route::post('/admin/editPassword', [AdminController::class, 'editPassword']);
+    Route::get('/admin/me', [AdminController::class, 'me']);
 });
